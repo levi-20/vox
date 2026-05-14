60215310154e8c18c9e01cd9257c54a5f8d969f6
levi-20 <shekhar.3706@gmail.com>
2026-05-14 18:40:16 +0530

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Navigation from "./components/navigation";
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
-        <Route path="/" element={<h1>Vox</h1>}/>
-        <Route path="/login" element={<div>Login Page</div>}></Route>
-        <Route path="/register" element={<div>Register Page</div>}></Route>
-        <Route path="/rooms" element={<div>Rooms Page</div>}></Route>
-        <Route path="/rooms/:roomId" element={<div>Room Chat Page</div>}></Route>
-        <Route path="/policies" element={<div>Policy Page</div>}></Route>
-        <Route path="/contact-us" element={<div>Contact us Page</div>}></Route>
+        <Route path="/" element={<h1>Vox</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<div>Register Page</div>} />
+        <Route path="/rooms" element={<div>Rooms Page</div>} />
+        <Route path="/rooms/:roomId" element={<div>Room Chat Page</div>} />
+        <Route path="/policies" element={<div>Policy Page</div>} />
+        <Route path="/contact-us" element={<div>Contact us Page</div>} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/navigation.jsx
+++ frontend/src/components/navigation.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Navigation() {
-  return (
-    <nav>
-      <a href="/login">Login</a>
-      <a href="/register">Register</a>
-    </nav>
-  )
+	return (
+		<nav className="navigation">
+
+			<a href="/" className="nav-logo">Vox</a>
+
+			<ul className="nav nav-container">
+				<li className="nav-item"><Link to="/login">Login</Link></li>
+				<li className="nav-item"><Link to="/register">Register</Link></li>
+			</ul>
+		</nav>
+	)
 }
 
